2ce696c18076a4f745165ce3cfab6c5997555a4e
Ezra Halberstadt <ethalberstadt@gmail.com>
2024-09-04 14:43:56 -0500

    Final update to application.js added create product functionality

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -3,25 +3,56 @@ $(document).ready(function () {
   });
 
   var updateSubTotalPrice = function (ele) {
-    var cost = parseFloat($(ele).children('.cost').text());
+    var cost = parseFloat($(ele).children('.cost').children(".costSpan").text());
     var quantity = $(ele).children(".quantityRow").children('.quantity').val();  
     var subTotalPrice = cost * quantity;
-    $(ele).children('.price').text(subTotalPrice);
+
+    $(ele).children('.price').text("$" + subTotalPrice);
 
     return subTotalPrice;
   }
 
-var sum = function (acc, x) { return acc + x};
+var sum = function (acc, x) { return Number(acc) + Number(x)};
  
 $(document).ready(function () {
     updateTotalPrice();
 
 
-  $(".cancel").on('click', function (event) {
-    $(this).closest('tr').remove();
+  //$(".cancel").on('click', function (event) {
+    //$(this).closest('tr').remove();
+    //updateTotalPrice();
+    //});
+    $(document).on('click', '.cancel', function (event) {
+      $(this).closest('tr').remove();
+      updateTotalPrice();
+    });
+    
+    var timeout;
+    $(document).on('input', 'tr input', function () {
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        updateTotalPrice();
+      }, 500);
     });
 
-  $('.quantity').on('input', function () {
+  //$('.quantity').on('input', function () {
+ //   updateTotalPrice();
+  //});
+
+
+  $('#addProduct').on('submit', function (event) {
+    event.preventDefault();
+    var name = $(this).children('[name=name]').val();
+    var cost = $(this).children('[name=cost]').val();
+
+      $('tbody').append('<tr>' +
+    '<td class="name">' + name + '</td>' +
+    '<td class="cost">$<span class="costSpan">' + cost + '</td>' +
+    '<td class="quantityRow"><b>QTY</b><input class="quantity" type="number" value="1" /><button class="btn btn-light btn-sm cancel" style="border: thin;">Cancel</button></td>' + '<td class="price">20</td>' +
+  '</tr>');
+  $('tbody tr').each(function (i, ele) {
+    updateSubTotalPrice($(ele));
+  });
     updateTotalPrice();
   });
   });
@@ -35,6 +66,5 @@ $(document).ready(function () {
     });
   
   var totalPrice = subTotalValues.reduce(sum);
-  console.log(totalPrice);
   $('#totalPriceSpan').html(totalPrice);
   }
